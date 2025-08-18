2ea2d1e2036a2ed4d16c5e0a57026e4f77c57ae5
aya69 <156995935+ayaamir99@users.noreply.github.com>
2025-08-18 12:13:46 +0000

    automatic fixture

--- tests/customFixtures.ts
+++ tests/customFixtures.ts
@@ -2,11 +2,23 @@ import {test as base  , expect} from '@playwright/test'
 // custom fixture
 const test = base.extend({
     testData : async({},use) => {
-        const data ={email : "ayaamer@test.com"
-            , password:"74108520"
+        const data ={email : "ayaamer@test.com", password:"74108520"
         }
         await use(data)
     }
+    //call back function => use
+    ,authenticatedUser : [async({page,testData},use)=>{
+    await page.goto("10.10.0.18");
+    const emailInput = page.getByRole("textbox",{name:"Email"});
+    await emailInput.fill(testData.email)
+     const passwordInput = page.getByRole("textbox",{name:"Password"});
+    await passwordInput.fill(testData.password)
+     const SignInButton = page.getByRole("button",{name:"Sign In"});
+    await SignInButton.click()
+
+    await use{page}
+    },{auto: true}]
+// to tell code that is automatic fixture
     
 })
 test("should able to login",async({page,testData}) =>{
@@ -15,5 +27,7 @@ test("should able to login",async({page,testData}) =>{
     await emailInput.fill(testData.email)
      const passwordInput = page.getByRole("textbox",{name:"Password"});
     await passwordInput.fill(testData.password)
+     const SignInButton = page.getByRole("button",{name:"Sign In"});
+    await SignInButton.click()
 }
 )
